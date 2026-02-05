5276c9a9f29a1993f257f8c57d01ecad3ddf0113
Pranav-veyondtech <keval.infinitysoftech@gmail.com>
2026-02-05 11:45:10 +0530

    added link to redirect to news page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,10 @@
+import { NavLink } from "react-router";
+
 export default function Home() {
   return (
     <section className="page">
-      <h2>Home Page</h2>
+      <h1>Home Page</h1>
+      <NavLink to="/news" style={{color:'blue',textDecoration:'underline',fontSize:'40px'}}>Go to News & Blog page</NavLink>
     </section>
   );
 }
