1e67b6b270f8c4cd0effd8d7dded021b2d448506
Pranav-veyondtech <keval.infinitysoftech@gmail.com>
2026-02-05 10:43:45 +0530

    impimented global spinner for news page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Outlet, useNavigation } from "react-router";
+import { Outlet, useNavigation, useLocation } from "react-router";
 import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -6,13 +6,31 @@ import Spinner from "./components/Spinner";
 
 function App() {
   const navigate = useNavigation();
+  const location = useLocation();
   const isLoading = navigate.state === "loading";
+
+  // Show spinner only when loading and NOT already on news page
+  const shouldShowAppSpinner =
+    isLoading && !location.pathname.startsWith("/news");
 
   return (
     <div className="container">
       <Header />
-      {/* {isLoading ? <Spinner /> : <Outlet />} */}
-      <Outlet/>
+      {shouldShowAppSpinner ? (
+        <div
+          style={{
+            padding: "150px 20px",
+            display: "grid",
+            placeItems: "center",
+            // minHeight: "60vh",
+          }}
+        >
+          <Spinner />
+          <h2 style={{ fontSize: "40px", color: "#666", marginTop: "40px" }}>Loading...</h2>
+        </div>
+      ) : (
+        <Outlet />
+      )}
       <Footer />
     </div>
   );
