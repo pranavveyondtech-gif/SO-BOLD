964e20e5c6676a366723ade4c848d5820488a92b
Pranav-veyondtech <keval.infinitysoftech@gmail.com>
2026-02-18 09:32:18 +0530

    wrote developed page link to home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,28 @@ export default function Home() {
   return (
     <section className="page">
       <h1>Home Page</h1>
-      <NavLink to="/news" style={{color:'blue',textDecoration:'underline',fontSize:'40px'}}>Go to News & Blog page</NavLink>
+      <h6 style={{ margin: "30px 0 20px" }}>Curently developed pages:</h6>
+
+      <NavLink
+        to="/news"
+        style={{ color: "blue", textDecoration: "underline", fontSize: "25px" }}
+      >
+        News & Blog
+      </NavLink>
+
+      <NavLink
+        to="/privacy-policy"
+        style={{ color: "blue", textDecoration: "underline", fontSize: "25px" }}
+      >
+        Privacy Policy
+      </NavLink>
+
+      <NavLink
+        to="/faqs"
+        style={{ color: "blue", textDecoration: "underline", fontSize: "25px" }}
+      >
+        FAQs
+      </NavLink>
     </section>
   );
 }
